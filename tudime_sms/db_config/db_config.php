07c5f68fe944920754d85d42577f7a34518203a0
muhammad.bakhtiyar <muhammad.bakhtiyar@tenpearls.com>
2021-09-12 22:44:03 +0500

    referal deletion schedule scripts are completed

--- tudime_sms/db_config/db_config.php
+++ tudime_sms/db_config/db_config.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("localhost","root","");
+    $con=mysqli_connect("localhost","root","IpTv@2019");
     $db=mysqli_select_db($con,"tudime_sms");
 ?>
 
--- tudime_sms/referal_deletion_schedule.php
+++ tudime_sms/referal_deletion_schedule.php
@@ -3,9 +3,10 @@
     include 'db_config/db_config.php';
 
     function deleteOldReferal(){
-        $sql = "INSERT INTO `user_referal`(`useid`, `referal_username`)VALUES('2', 'bakhti') ";
+        $threeDaysPreviousDate = date('Y-m-d', strtotime('-3 days', strtotime(date('Y-m-d H:i:s'))));
+        $sql = "DELETE FROM `user_referal` WHERE `timestamp` < '".$threeDaysPreviousDate."' ";
         mysqli_query($GLOBALS['con'], $sql);
-        $response = array("status" => "success", "error_message" => "", "success_message" => "insert successfull.", "data" => "");
+        $response = array("status" => "success", "error_message" => "", "success_message" => "Deletion Success.", "data" => "");
         echo json_encode($response);
     }
 
